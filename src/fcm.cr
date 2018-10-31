498be46dfca6c39fc162681d5fedb635b4426b02
Kerem <kerem@noras.tech>
2018-10-31 22:19:51 +0300

    ~base uri, ~initialize<-@options(!)>, ~http header key

--- src/fcm.cr
+++ src/fcm.cr
@@ -4,18 +4,18 @@ require "http/client"
 
 module Fcm
   class Base
-    getter :base_uri, "https://fcm.googleapis.com/fcm"
-    getter :timeout, 30
-    getter :format, :json
+    getter base_uri = "https://fcm.googleapis.com/fcm/"
+    getter timeout = 30
+    getter format = :json
 
-    def initialize(@api_key : String, @options : {} of String)
+    def initialize(@api_key : String)
     end
 
-    def send(registration_id, options : {} of String)
+    def send(registration_id, options : Hash(String, String))
       message = { "to" => registration_id }.merge(options)
       response = HTTP::Client.post(
-        @base_uri,
-        headers: HTTP::Headers {"Authorzation" => "key=#{@api_key}", "Content-Type" => "application/json" },
+        @base_uri + "send",
+        headers: HTTP::Headers {"Authorization" => "key=#{@api_key}", "Content-Type" => "application/json" },
         body: message.to_json
       )
     end
